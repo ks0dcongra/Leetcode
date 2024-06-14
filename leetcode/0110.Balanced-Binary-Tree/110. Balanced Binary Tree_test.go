181034fe737ca6cee320bb59631306ce1d47ae6d
ks0dcongra <ks0dcongra@gmail.com>
2024-06-14 16:52:05 +0800

    Update leetcode questioins

--- leetcode/0110.Balanced-Binary-Tree/110. Balanced Binary Tree_test.go
+++ leetcode/0110.Balanced-Binary-Tree/110. Balanced Binary Tree_test.go
@@ -5,9 +5,11 @@ import (
 	"testing"
 
 	"github.com/halfrost/LeetCode-Go/structures"
+	"github.com/stretchr/testify/assert"
 )
 
 type question110 struct {
+	name string
 	para110
 	ans110
 }
@@ -29,36 +31,43 @@ func Test_Problem110(t *testing.T) {
 	qs := []question110{
 
 		{
+			"1",
 			para110{[]int{3, 4, 4, 5, structures.NULL, structures.NULL, 5, 6, structures.NULL, structures.NULL, 6}},
 			ans110{false},
 		},
 
 		{
+			"2",
 			para110{[]int{1, 2, 2, structures.NULL, 3, 3}},
 			ans110{true},
 		},
 
 		{
+			"3",
 			para110{[]int{}},
 			ans110{true},
 		},
 
 		{
+			"4",
 			para110{[]int{1}},
 			ans110{true},
 		},
 
 		{
+			"5",
 			para110{[]int{1, 2, 3}},
 			ans110{true},
 		},
 
 		{
+			"6",
 			para110{[]int{1, 2, 2, 3, 4, 4, 3}},
 			ans110{true},
 		},
 
 		{
+			"7",
 			para110{[]int{1, 2, 2, structures.NULL, 3, structures.NULL, 3}},
 			ans110{true},
 		},
@@ -66,11 +75,18 @@ func Test_Problem110(t *testing.T) {
 
 	fmt.Printf("------------------------Leetcode Problem 110------------------------\n")
 
+	// for _, q := range qs {
+	// 	_, p := q.ans110, q.para110
+	// 	fmt.Printf("【input】:%v      ", p)
+	// 	rootOne := structures.Ints2TreeNode(p.one)
+	// 	fmt.Printf("【output】:%v      \n", isBalanced(rootOne))
+	// }
 	for _, q := range qs {
-		_, p := q.ans110, q.para110
-		fmt.Printf("【input】:%v      ", p)
-		rootOne := structures.Ints2TreeNode(p.one)
-		fmt.Printf("【output】:%v      \n", isBalanced(rootOne))
+		t.Run(q.name, func(t *testing.T) {
+			a, p := q.ans110, q.para110
+			rootOne := structures.Ints2TreeNode(p.one)
+			assert.Equal(t, a.one, isBalanced(rootOne))
+		})
 	}
 	fmt.Printf("\n\n\n")
 }
